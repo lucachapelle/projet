8d0b1514de0c40602de08b30d40054f3d532f07e
lucachapelle <36231726+lucachapelle@users.noreply.github.com>
2019-01-25 18:43:56 +0100

    1

---
+++ android/Assets/testbdd.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testbdd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button button;
+    private int i = 1;
+    void Start()
+    {
+        //Calls the TaskOnClick/TaskWithParameters/ButtonClicked method when you click the Button
+        button.onClick.AddListener(() => ButtonClicked(i));
+        
+    }    
+    
+    void ButtonClicked(int buttonNo)
+    {
+        //Output this to console when the Button3 is clicked
+        StartCoroutine(test());
+       
+    }
+
+    IEnumerator test()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("id",2);
+        WWW www = new WWW("https://primsie-spears.000webhostapp.com/test.php",form);
+        yield return www;
+        if (www.text == "0")
+        {
+            Debug.Log("Il n y a pas de probleme");
+
+        }
+        else
+        {
+            Debug.Log("error"+www.text);
+
+        }
+    }
+ }
